22ee2793a203f7824608c98ade328829c6bfce34
Helnyr <Helnyr@users.noreply.github.com>
2020-10-29 13:33:51 +0100

    Comentaris al main y player

--- scripts/player.lua
+++ scripts/player.lua
@@ -14,16 +14,16 @@ function player:new(x, y)
   self.sprite = love.graphics.newImage("sprites/Tarma Idle.png")
   self.spriteScale = 1
   self.forward = Vector.new(fx or 1,fy or 0)
-  self.airborn = true
+  self.airborn = true --El personatge esta al aire
   
   self.currentTorsoAnimation = 1
   self.torsoSpriteSheet = love.graphics.newImage('sprites/Torso_Sandra_Spritesheet.png')
   g = anim8.newGrid(54, 32, self.torsoSpriteSheet:getWidth(), self.torsoSpriteSheet:getHeight())
-  self.torsoAnimations = {anim8.newAnimation(g('1-12',1), 0.07),--RUNNING
-                          anim8.newAnimation(g('1-10',2), 0.03),--SHOOTING
-                          anim8.newAnimation(g('1-6',3), 0.15),--IDLE
-                          anim8.newAnimation(g('1-6',4), 0.1) --JUMPING
-    }
+  self.torsoAnimations = {anim8.newAnimation(g('1-12',1), 0.07),--RUNNING (1r Valor: Rang de frames. 2n Valor: Fila del sheet)
+                          anim8.newAnimation(g('1-10',2), 0.03),--SHOOTING (1r Valor: Rang de frames. 2n Valor: Fila del sheet)
+                          anim8.newAnimation(g('1-6',3), 0.15),--IDLE (1r Valor: Rang de frames. 2n Valor: Fila del sheet)
+                          anim8.newAnimation(g('1-6',4), 0.1) --JUMPING (1r Valor: Rang de frames. 2n Valor: Fila del sheet)
+    } --3r Valor: Velocitat de la animació
     
   self.currentLegsAnimation = 1
   self.legsSpriteSheet = love.graphics.newImage('sprites/Legs_Sandra_Spritesheet.png')
@@ -43,10 +43,10 @@ function player:new(x, y)
   objects.player.body:setFixedRotation(true)
   
   --Player shooting
-  self.shooting = false
-  self.shot = false
+  self.shooting = false --El personatge esta disparant
+  self.shot = false --El personatge ja ha disparat la bala
   self.fireRate = 0.2
-  self.nextFire = 0
+  self.nextFire = 0 --Timer, se li sumara dt fins arribar a fireRate
 end
 
 function player:update(dt)
@@ -69,35 +69,37 @@ function player:update(dt)
     if not self.shooting then
       self.currentTorsoAnimation = 1
     end
-  else if not self.airborn then
-    if not self.shooting then
+  else if not self.airborn then --Si el personatge no esta al aire i no s'esta movent
+    if not self.shooting then --Ni esta disparant
       self.currentTorsoAnimation = 3
     end
-      self:changeLegsAnimation(1)
+      self:changeLegsAnimation(1) --Testing
     end
   end
   
+  --En proces: Que el miri cap a dalt
   if love.keyboard.isDown("up") then --press the up arrow key to set the ball in the air
     self.forward.y = -1
   else
     self.forward.y = 0
-    
   end
   
+  --Vector que agafa la velocitat en X i Y del personatge
   self.velocity = Vector.new(objects.player.body:getLinearVelocity())
   
   if love.keyboard.isDown("space") and canJump then
-    if not self.airborn then
-      canJump = false
+    canJump = false
+    
+    if not self.airborn then --Si esta tocant a terre
       objects.player.body:setLinearVelocity(objects.player.body:getLinearVelocity(x, y))
       objects.player.body:applyLinearImpulse(0, self.jumpForce)
       
       self:changeLegsAnimation(3)
       
       if not self.shooting then
-      self.currentTorsoAnimation = 4
-      self.torsoAnimations[self.currentTorsoAnimation]:gotoFrame(1)
-      self.torsoAnimations[self.currentTorsoAnimation]:resume()
+        self.currentTorsoAnimation = 4
+        self.torsoAnimations[self.currentTorsoAnimation]:gotoFrame(1)
+        self.torsoAnimations[self.currentTorsoAnimation]:resume()
       end
     end
   elseif not love.keyboard.isDown("space") then
@@ -105,9 +107,7 @@ function player:update(dt)
   end
   
   if self.velocity.y > -0.01 and self.velocity.y < 0.01 then
-    if self.airborn then
-      self.airborn = false
-    end
+    self.airborn = false
   else
     self.airborn = true
     self.currentLegsAnimation = 3
@@ -116,6 +116,7 @@ function player:update(dt)
       self.currentTorsoAnimation = 4
     end
     
+    --Para que la animacion de salto no haga loop
     if self.legsAnimations[self.currentLegsAnimation]:getCurrentFrameCounter() == self.legsAnimations[self.currentLegsAnimation]:getTotalFrameCounter() then
       self.legsAnimations[self.currentLegsAnimation]:pauseAtEnd()
     end
@@ -137,6 +138,7 @@ function player:update(dt)
     end
   end
   
+  --Coordinar el spawn de la bala amb el moment de la animacio que li toca
   if self.shooting and not self.shot and self.torsoAnimations[self.currentTorsoAnimation]:getCurrentFrameCounter() == 3 then
     b = bullet:extend()
     b:new(objects.player.body:getX(), objects.player.body:getY(), self.forward)
@@ -145,15 +147,15 @@ function player:update(dt)
     print(self.torsoAnimations[self.currentTorsoAnimation]:getCurrentFrameCounter())
   elseif self.shooting and self.torsoAnimations[self.currentTorsoAnimation]:getCurrentFrameCounter() == self.torsoAnimations[self.currentTorsoAnimation]:getTotalFrameCounter() then
     self.shooting = false
-    print("finish")
   end
   
+  --Temps de recarrega per tornar a disparar
   self.nextFire = self.nextFire + dt
-  
   if self.nextFire >= self.fireRate then
     canShoot = true
   end
   
+  --Fa que funcioni el update del anim8
   for i=1,#self.legsAnimations do
     self.legsAnimations[i]:update(dt)
   end
